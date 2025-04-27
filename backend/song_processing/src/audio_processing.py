34427e029a2c45830cbd3d28d421b65fca954ac2
Aamir Sahil <notifysahil@gmail.com>
2025-04-27 11:42:11 +0530

    added doc string to isolate_music function

--- backend/song_processing/src/audio_processing.py
+++ backend/song_processing/src/audio_processing.py
@@ -2,4 +2,10 @@ class AudioProcessing:
     def __init__(self):
         pass
     def isolate_music(self):
+        """
+        Audio track is processed and vocal tracks are removed.
+        The isolated music is then returned.
+        param:
+        return:
+        """
         pass
